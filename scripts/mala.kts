fad6e4ac7d2b8027d3db2a6f5ae7d38ae698c7e9
Fabrice Jossinet <fjossinet@gmail.com>
2021-11-24 11:38:52 +0100

    Added the mala instruction to the DSL. Mala means painting in swedish.

---
+++ scripts/mala.kts
@@ -0,0 +1,17 @@
+import io.github.fjossinet.rnartist.core.*
+
+spiraltree {
+    file = "/Users/fjossinet/tmp/RF02001.svg"
+    selection = "175:86"
+    ss {
+        stockholm {
+            file = "samples/RF02001.stockholm"
+        }
+        
+    }
+
+    theme {
+        details_lvl = 2
+    }
+
+}
